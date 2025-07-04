60831e8e8591b8a583dc1a4b5b7a7bc20b05b9a7
HTwebsitesolution <htwebsitesolution@gmail.com>
2025-07-04 02:32:38 +0100

    Update Deal.js

--- models/Deal.js
+++ models/Deal.js
@@ -1,1 +1,12 @@
- 
+const mongoose = require('mongoose');
+
+const DealSchema = new mongoose.Schema({
+  deal_title: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: String, required: true },
+  country: { type: String, required: true },
+  deal_link: { type: String, required: true },
+  expiry_date: { type: Date },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Deal', DealSchema);  
