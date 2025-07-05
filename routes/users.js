7a8a495d625c9f556d287832ddc3068d39927367
HTwebsitesolution <htwebsitesolution@gmail.com>
2025-07-05 13:07:38 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  favourites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Deal' }],
-}, { timestamps: true });
+// GET all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
-module.exports = mongoose.model('User', UserSchema);  
+// POST create user
+router.post('/', async (req, res) => {
+  const { username, email } = req.body;
+  if (!username || !email) {
+    return res.status(400).json({ error: 'Username and email required' });
+  }
+  const newUser = new User({ username, email });
+  try {
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+module.exports = router;
